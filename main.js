993e48c200f67f217488edb0ab808d24a1ee6b7b
Mustafa Dalga <mustafadalgaa@gmail.com>
2021-01-08 00:59:34 +0300

    Skeletion placeholder example

---
+++ main.js
@@ -0,0 +1,11 @@
+const card = document.querySelector('.card');
+const fetchImage = () => {
+    fetch('https://source.unsplash.com/random/24000*24000')
+        .then(response => {
+            card.style.background = `url(${response.url})`
+            card.classList.remove('placeholder')
+        })
+}
+setTimeout(() => {
+    fetchImage()
+}, 2000);

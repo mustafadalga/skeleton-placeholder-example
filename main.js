c29e55f9f3e8a5c31a5f4aca7fb9f81169b1b7d4
Mustafa Dalga <25087769+mustafadalga@users.noreply.github.com>
2021-04-04 15:26:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const card = document.querySelector('.card');
 const fetchImage = () => {
     fetch('https://source.unsplash.com/random/24000*24000')
         .then(response => {
-            card.style.background = `url(${response.url}) center/cover no-repeat`
+            card.style.backgroundImage = `url(${response.url})`;
             card.classList.remove('placeholder')
         })
 }
